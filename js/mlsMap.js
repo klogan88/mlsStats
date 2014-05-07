1d2bd19023d06c24602796b7b45ad83a6ae7277b
Kevin Logan <klogan888@gmail.com>
2014-05-07 15:47:03 -0700

    added requirejs and modularized js code

--- js/mlsMap.js
+++ js/mlsMap.js
@@ -1,149 +1,160 @@
-//Stats view is initally hidden.
-$("#statsContainer").hide();
-
-//Radius for the circles on the map.
-var circleRadius = 15;
-
-//The team location data for placing the bubbles on the map.
-var teamLocations = [
-	{abbr: 'DCU', name: 'D.C. United', latitude: 38.8951, longitude: -77.0367, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'NYRB', name: 'New York Red Bulls', latitude: 40.6700, longitude: -73.9400, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'PHI', name: 'Philadelphia Union', latitude: 39.9500, longitude: -75.1700, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'CLB', name: 'Columbus Crew', latitude: 39.9833, longitude: -82.9833, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'SKC', name: 'Sporting Kansas City', latitude: 39.0997, longitude: -94.5786, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'CHI', name: 'Chicago Fire', latitude: 41.8819, longitude: -87.6278, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'TFC', name: 'Toronto FC', latitude: 43.7000, longitude: -79.4000, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'MTL', name: 'Montreal Impact', latitude: 45.5000, longitude: -73.5667, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'NE', name: 'New England Revolution', latitude: 42.3581, longitude: -71.0636, radius: circleRadius, fillKey: 'east'},
-	{abbr: 'HOU', name: 'Houston Dynamo', latitude: 29.7628, longitude: -95.3831, radius: circleRadius, fillKey: 'east'}, 
-	{abbr: 'FCD', name: 'FC Dallas', latitude: 32.7758, longitude: -96.7967, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'LAG/CHV', name: 'Los Angeles Galaxy/Chivas USA', latitude: 34.0500, longitude: -118.2500, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'SJ', name: 'San Jose Earthquakes', latitude: 37.3333, longitude: -121.9000, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'SEA', name: 'Seattle Sounders FC', latitude: 47.6097, longitude: -122.3331, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'POR', name: 'Portland Timbers', latitude: 45.5200, longitude: -122.6819, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'VAN', name: 'Vancouver Whitecaps FC', latitude: 49.2500, longitude: -123.1000, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'RSL', name: 'Real Salt Lake', latitude: 40.7500, longitude: -111.8833, radius: circleRadius, fillKey: 'west'},
-	{abbr: 'COL', name: 'Colorado Rapids', latitude: 39.7392, longitude: -104.9847, radius: circleRadius, fillKey: 'west'}
-];
-
-//Separated data for LA Galaxy and Chivas to use later since they are located in the same city/stadium.
-var chvLaData = [
-	{abbr: 'LAG', name: 'Los Angeles Galaxy', fillKey: 'west'},
-	{abbr: 'CHV', name: 'Chivas USA', fillKey: 'west'}
-];
-
-//Set up the datamap to be focuse on USA and Canada, the countries MLS is located in.
-var map = new Datamap({
-	element: document.getElementById('map'),
-	scope: 'world',
-	setProjection: function(element) {
-		var projection = d3.geo.equirectangular()
-		  .center([-40, 10])
-		  .rotate([4.4, 0])
-		  .scale(800)
-		  .translate([element.offsetWidth, element.offsetHeight]);
-		var path = d3.geo.path()
-		  .projection(projection);
-		
-		return {path: path, projection: projection};
+require({
+		paths: {
+			jquery: 'js/jquery',
+			utils: 'js/mlsUtils'
+		}
 	},
-	geographyConfig: {
-		highlightOnHover: false,
-		popupOnHover: false
-	},
-	fills: {
-		defaultFill: "#ABDDA4",
-		east: '#5FABED',
-		west: '#EDA15F'
-    },
-});
-
-//Populate the bubbles on the map.
-map.bubbles(teamLocations, {
-	fillOpacity: 1.00,
-	popupTemplate: function(geo, data) {
-	  var imageHTML = "";
-	  var imageFile = "";
-	  if(data.abbr === "LAG/CHV") {
-		imageHTML = "<div><img src='img/LAG.png' width='50px' height='50px' class='centerImage'><img src='img/CHV.png' width='50px' height='50px' class='centerImage'></div>"
-	  } else {
-	    imageFile = "img/" + data.abbr + ".png";
-		imageHTML = "<img src='" + imageFile + "' width='50px' height='50px' class='centerImage'>";
-	  }
-	  return "<div class='hoverinfo'><div>" + data.name + "</div>" + imageHTML + "</div>";
-	}
-});
-
-//Add click event for the back button when on the stats view.  Goes back to map view.
-$("#backBtn").click(function() {
-	$("#statsContainer").fadeOut('slow', function() {
-		d3.selectAll("#goalBarChart svg").remove();
-		d3.selectAll("#ptsBarChart svg").remove();
-		d3.selectAll("#seasonTable table").remove();
-		$("#mapContainer").fadeIn('slow');
-		$("#map svg").fadeIn('slow');
-	});
-});
-
-//Add click event for Okay button to selecte LA or Chivas when selected.
-$("#okBtn").click(function() {
-	if($("#lagRadio").prop("checked") === true) {
-		openStats(chvLaData[0]);
-	} else {
-		openStats(chvLaData[1]);
-	}
+	['jquery', 'utils'],
+	function($, utils){
 	
-	$("#selectorDialog").fadeOut();
-	$("#overlay").fadeOut();
+		//Stats view is initally hidden.
+		$("#statsContainer").hide();
+
+		//Radius for the circles on the map.
+		var circleRadius = 15;
+
+		//The team location data for placing the bubbles on the map.
+		var teamLocations = [
+			{abbr: 'DCU', name: 'D.C. United', latitude: 38.8951, longitude: -77.0367, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'NYRB', name: 'New York Red Bulls', latitude: 40.6700, longitude: -73.9400, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'PHI', name: 'Philadelphia Union', latitude: 39.9500, longitude: -75.1700, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'CLB', name: 'Columbus Crew', latitude: 39.9833, longitude: -82.9833, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'SKC', name: 'Sporting Kansas City', latitude: 39.0997, longitude: -94.5786, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'CHI', name: 'Chicago Fire', latitude: 41.8819, longitude: -87.6278, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'TFC', name: 'Toronto FC', latitude: 43.7000, longitude: -79.4000, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'MTL', name: 'Montreal Impact', latitude: 45.5000, longitude: -73.5667, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'NE', name: 'New England Revolution', latitude: 42.3581, longitude: -71.0636, radius: circleRadius, fillKey: 'east'},
+			{abbr: 'HOU', name: 'Houston Dynamo', latitude: 29.7628, longitude: -95.3831, radius: circleRadius, fillKey: 'east'}, 
+			{abbr: 'FCD', name: 'FC Dallas', latitude: 32.7758, longitude: -96.7967, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'LAG/CHV', name: 'Los Angeles Galaxy/Chivas USA', latitude: 34.0500, longitude: -118.2500, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'SJ', name: 'San Jose Earthquakes', latitude: 37.3333, longitude: -121.9000, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'SEA', name: 'Seattle Sounders FC', latitude: 47.6097, longitude: -122.3331, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'POR', name: 'Portland Timbers', latitude: 45.5200, longitude: -122.6819, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'VAN', name: 'Vancouver Whitecaps FC', latitude: 49.2500, longitude: -123.1000, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'RSL', name: 'Real Salt Lake', latitude: 40.7500, longitude: -111.8833, radius: circleRadius, fillKey: 'west'},
+			{abbr: 'COL', name: 'Colorado Rapids', latitude: 39.7392, longitude: -104.9847, radius: circleRadius, fillKey: 'west'}
+		];
+
+		//Separated data for LA Galaxy and Chivas to use later since they are located in the same city/stadium.
+		var chvLaData = [
+			{abbr: 'LAG', name: 'Los Angeles Galaxy', fillKey: 'west'},
+			{abbr: 'CHV', name: 'Chivas USA', fillKey: 'west'}
+		];
+
+		//Set up the datamap to be focuse on USA and Canada, the countries MLS is located in.
+		var map = new Datamap({
+			element: document.getElementById('map'),
+			scope: 'world',
+			setProjection: function(element) {
+				var projection = d3.geo.equirectangular()
+				  .center([-40, 10])
+				  .rotate([4.4, 0])
+				  .scale(800)
+				  .translate([element.offsetWidth, element.offsetHeight]);
+				var path = d3.geo.path()
+				  .projection(projection);
+				
+				return {path: path, projection: projection};
+			},
+			geographyConfig: {
+				highlightOnHover: false,
+				popupOnHover: false
+			},
+			fills: {
+				defaultFill: "#ABDDA4",
+				east: '#5FABED',
+				west: '#EDA15F'
+			},
+		});
+
+		//Populate the bubbles on the map.
+		map.bubbles(teamLocations, {
+			fillOpacity: 1.00,
+			popupTemplate: function(geo, data) {
+			  var imageHTML = "";
+			  var imageFile = "";
+			  if(data.abbr === "LAG/CHV") {
+				imageHTML = "<div><img src='img/LAG.png' width='50px' height='50px' class='centerImage'><img src='img/CHV.png' width='50px' height='50px' class='centerImage'></div>"
+			  } else {
+				imageFile = "img/" + data.abbr + ".png";
+				imageHTML = "<img src='" + imageFile + "' width='50px' height='50px' class='centerImage'>";
+			  }
+			  return "<div class='hoverinfo'><div>" + data.name + "</div>" + imageHTML + "</div>";
+			}
+		});
+
+		//Add click event for the back button when on the stats view.  Goes back to map view.
+		$("#backBtn").click(function() {
+			$("#statsContainer").fadeOut('slow', function() {
+				d3.selectAll("#goalBarChart svg").remove();
+				d3.selectAll("#ptsBarChart svg").remove();
+				d3.selectAll("#seasonTable table").remove();
+				$("#mapContainer").fadeIn('slow');
+				$("#map svg").fadeIn('slow');
+			});
+		});
+
+		//Add click event for Okay button to selecte LA or Chivas when selected.
+		$("#okBtn").click(function() {
+			if($("#lagRadio").prop("checked") === true) {
+				openStats(chvLaData[0]);
+			} else {
+				openStats(chvLaData[1]);
+			}
+			
+			$("#selectorDialog").fadeOut();
+			$("#overlay").fadeOut();
+		});
+
+		//Add click event for cancel button to pick neither LA or Chivas and return to map.
+		$("#cancelBtn").click(function() {
+			$("#selectorDialog").fadeOut();
+			$("#overlay").fadeOut();
+		});
+
+		//Add the events to the circles so that they are clickable on the map.
+		addEvents();
+
+		//Implement click events on the bubbles.
+		function addEvents(){
+			var circles = d3.select("#map > svg").selectAll("circle")
+			   .on("click", function(d) { clickCircle(d); });
+		};
+
+		//Depending on circle clicked, either open LAG/CHV dialog or stats view.
+		function clickCircle(d) {
+
+			if(d.abbr === "LAG/CHV") {
+				openDialog();		
+			} else {
+				openStats(d); 
+			}
+		};
+
+		//Opens stats view and hides map view.
+		function openStats(d) {
+			$("#mapContainer").fadeOut('slow', function() {
+				$("#teamLabel").text(d.name);
+				utils.buildBar("#goalBarChart", "mlsStats.tsv", d.abbr, "Goals", "Team");
+				utils.buildBar("#ptsBarChart", "mlsPts.tsv", d.abbr, "Points", "Team");
+				utils.buildSeasonTable(d);
+				$("#statsContainer").fadeIn('slow');
+			}); 
+		};
+
+		//Open the dialog to select either LA or Chivas.
+		function openDialog() {
+			var body = $(window),
+				bodyWidth = body.width(),
+				bodyHeight = body.height(),
+				selector = $("#selectorDialog"),
+				dialogWidth = selector.width(),
+				dialogHeight = selector.height(),
+				left = (bodyWidth - dialogWidth)/2,
+				top = (bodyHeight - dialogHeight)/2;
+				
+			selector.css('left', left).css('top', top).fadeIn();
+			$("#overlay").fadeIn();
+		};
+	
 });
-
-//Add click event for cancel button to pick neither LA or Chivas and return to map.
-$("#cancelBtn").click(function() {
-	$("#selectorDialog").fadeOut();
-	$("#overlay").fadeOut();
-});
-
-//Add the events to the circles so that they are clickable on the map.
-addEvents();
-
-//Implement click events on the bubbles.
-function addEvents(){
-	var circles = d3.select("#map > svg").selectAll("circle")
-	   .on("click", function(d) { clickCircle(d); });
-};
-
-//Depending on circle clicked, either open LAG/CHV dialog or stats view.
-function clickCircle(d) {
-
-	if(d.abbr === "LAG/CHV") {
-		openDialog();		
-	} else {
-		openStats(d); 
-	}
-};
-
-//Opens stats view and hides map view.
-function openStats(d) {
-	$("#mapContainer").fadeOut('slow', function() {
-		$("#teamLabel").text(d.name);
-		buildBar("#goalBarChart", "mlsStats.tsv", d.abbr, "Goals", "Team");
-		buildBar("#ptsBarChart", "mlsPts.tsv", d.abbr, "Points", "Team");
-		buildSeasonTable(d);
-		$("#statsContainer").fadeIn('slow');
-	}); 
-};
-
-//Open the dialog to select either LA or Chivas.
-function openDialog() {
-	var body = $(window),
-		bodyWidth = body.width(),
-		bodyHeight = body.height(),
-		selector = $("#selectorDialog"),
-		dialogWidth = selector.width(),
-		dialogHeight = selector.height(),
-		left = (bodyWidth - dialogWidth)/2,
-		top = (bodyHeight - dialogHeight)/2;
-		
-	selector.css('left', left).css('top', top).fadeIn();
-	$("#overlay").fadeIn();
-};
--- js/mlsUtils.js
+++ js/mlsUtils.js
@@ -1,209 +1,223 @@
-//Builds the bar chart.  Can pass in args to select they type of data and axis names for the bar chart.
-function buildBar(id, dataUrl, selectedTeam, yAxisName, xAxisName) {
+define([], function() {
 
-	var margin = {top: 20, right: 20, bottom: 50, left: 40},
-    width = 750 - margin.left - margin.right,
-    height = 350 - margin.top - margin.bottom;
+	//Tooltip on mouseover in the bar charts.
+	function showLabel(d){
 
-	var x = d3.scale.ordinal()
-		.rangeRoundBands([0, width], .1);
-
-	var y = d3.scale.linear()
-		.range([height, 0]);
-
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left")
-		.ticks(10);
-
-	var svg = d3.select(id).insert("svg:svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-	  .append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	d3.tsv("data/" + dataUrl, type, function(error, data) {
-	  x.domain(data.map(function(d) { return d.x; }));
-	  y.domain([0, d3.max(data, function(d) { return d.y; })]);
-
-	  svg.append("g")
-		  .attr("class", "x axis")
-		  .attr("transform", "translate(0," + height + ")")
-		  .call(xAxis)
-		.append("text")
-		  .attr("x", width/2)
-		  .attr("y", 40)
-		  .style("text-anchor", "end")
-		  .text(xAxisName);
-
-	  svg.append("g")
-		  .attr("class", "y axis")
-		  .call(yAxis)
-		.append("text")
-		  .attr("y", -10)
-		  .style("text-anchor", "middle")
-		  .text(yAxisName);
-
-	  svg.selectAll(".bar")
-		  .data(data)
-		.enter().append("rect")
-		  .attr("class", function(d) {
-				if(d.x === selectedTeam) {
-					return "selectedBar";
-				}
-				
-				return "bar";
-		  })
-		  .attr("x", function(d) { return x(d.x); })
-		  .attr("width", x.rangeBand())
-		  .attr("y", function(d) { return y(d.y); })
-		  .attr("height", function(d) { return height - y(d.y); })
-		  .on("click", function(d) { barClick(d); })
-		  .on("mouseover", function(d){return showLabel(d);})
-		  .on("mouseout", function(d){return hideLabel();});
-	});
-};
-
-//Tooltip on mouseover in the bar charts.
-function showLabel(d){
-
-	var label = "Goals";
-	if(d.goals) {
-		label = "Points";
-	}
-	
-	var info = "<span id='clubLabel'>" + d.Club + "</span><br/><hr><span id='typeLabel'>" + label + ": </span><span id='typeValue'>" + d.y + "</span>";
-	var windowWidth = $(window).width();
-	$('#barTool').html(info).show();
-	$(this).mousemove(function (e) {
-		var testWidth = e.pageX + 20 + 320;
-		if (windowWidth > testWidth) {
-			var x = e.pageX + 20;
-			var y = e.pageY + 20;
-		}
-		else {
-			var x = e.pageX - 330;
-			var y = e.pageY + 20;
+		var label = "Goals";
+		if(d.goals) {
+			label = "Points";
 		}
 		
-		$('#barTool').css({
-			top:y + 'px',
-			left:x + 'px'
-		});
-	})
-};
-
-//Hides the bar tooltip.
-function hideLabel(){	
-	$("#barTool").hide();
-};
-
-//Builds the season table for the particular team selected.
-function buildSeasonTable(teamData) {
-
-	var abbr = "";
-	
-	if(teamData.abbr) {
-		abbr = teamData.abbr;
-	} else {
-		abbr = teamData.x;
-	}
-	
-	d3.tsv("data/" + abbr + "season.tsv", null, function(error, data) {
-
-		processResults(data, teamData);
-		
-		var columns = ["match", "date", "hteam", "result", "ateam", "loc"];
-		var table = d3.select("#seasonTable").append("table"),
-			tbody = table.append("tbody");
-			
-		var rows = tbody.selectAll("tr")
-			.data(data)
-			.enter()
-			.append("tr")
-				.attr("class", function(d) { 
-					return d.outcome; 
-				});
-			
-		var cells = rows.selectAll("td")
-			.data(function(row) {
-				return columns.map(function(column) {
-					return {column: column, value: row[column]};
-				});
-			})
-			.enter()
-			.append("td")
-				.text(function(d) { return d.value.trim(); });
-	});
-};
-
-//Implements the bar click functionality for the bar charts.  Will populate the season data based on bar clicked.
-function barClick(data, currTeam) {
-
-	d3.selectAll(".selectedBar")
-		.attr("class", "bar");
-	
-	d3.selectAll(".bar")
-		.attr("class", function(d) {
-			if(d.x === data.x) {
-				return "selectedBar";
-			} else {
-				return "bar";
+		var info = "<span id='clubLabel'>" + d.Club + "</span><br/><hr><span id='typeLabel'>" + label + ": </span><span id='typeValue'>" + d.y + "</span>";
+		var windowWidth = $(window).width();
+		$('#barTool').html(info).show();
+		$(this).mousemove(function (e) {
+			var testWidth = e.pageX + 20 + 320;
+			if (windowWidth > testWidth) {
+				var x = e.pageX + 20;
+				var y = e.pageY + 20;
 			}
-		});
-	
-	$("#teamLabel").text(data.Club);
-	
-	$("#seasonTable").fadeOut('slow', function() {
+			else {
+				var x = e.pageX - 330;
+				var y = e.pageY + 20;
+			}
+			
+			$('#barTool').css({
+				top:y + 'px',
+				left:x + 'px'
+			});
+		})
+	};
+
+	//Hides the bar tooltip.
+	function hideLabel(){	
+		$("#barTool").hide();
+	};
+
+	//Implements the bar click functionality for the bar charts.  Will populate the season data based on bar clicked.
+	function barClick(data, currTeam) {
+
+		d3.selectAll(".selectedBar")
+			.attr("class", "bar");
+		
+		d3.selectAll(".bar")
+			.attr("class", function(d) {
+				if(d.x === data.x) {
+					return "selectedBar";
+				} else {
+					return "bar";
+				}
+			});
+		
 		$("#teamLabel").text(data.Club);
-		d3.selectAll("#seasonTable table").remove();
-		buildSeasonTable(data);
-	});
-	
-	$("#seasonTable").fadeIn('slow');
-};
-
-//Determines which games were wins or losses for the selected team
-function processResults(data, teamData) {
-
-	var teamName = ""
-	
-	if(teamData.name) {
-		teamName = teamData.name;
-	} else {
-		teamName = teamData.Club;
-	}
-	
-	var i = 0;
-	var score = [];
-	var res = 0;
-	
-	for(i = 0; i < data.length; i++) {
-		score = data[i].result.trim().split("");
 		
-		var t = data[i].hteam.trim();
-		if(teamName === data[i].hteam.trim()) {
-			res = Number(score[0]) - Number(score[2]);
+		$("#seasonTable").fadeOut('slow', function() {
+			$("#teamLabel").text(data.Club);
+			d3.selectAll("#seasonTable table").remove();
+			buildTable(data);
+		});
+		
+		$("#seasonTable").fadeIn('slow');
+	};
+
+	//Determines which games were wins or losses for the selected team
+	function processResults(data, teamData) {
+
+		var teamName = ""
+		
+		if(teamData.name) {
+			teamName = teamData.name;
 		} else {
-			res = Number(score[2]) - Number(score[0]);
+			teamName = teamData.Club;
 		}
 		
-		if(res < 0) {
-			data[i].outcome = "loss";
-		} else if(res > 0) {
-			data[i].outcome = "win";
-		} else {
-			data[i].outcome = "draw";
+		var i = 0;
+		var score = [];
+		var res = 0;
+		
+		for(i = 0; i < data.length; i++) {
+			score = data[i].result.trim().split("");
+			
+			var t = data[i].hteam.trim();
+			if(teamName === data[i].hteam.trim()) {
+				res = Number(score[0]) - Number(score[2]);
+			} else {
+				res = Number(score[2]) - Number(score[0]);
+			}
+			
+			if(res < 0) {
+				data[i].outcome = "loss";
+			} else if(res > 0) {
+				data[i].outcome = "win";
+			} else {
+				data[i].outcome = "draw";
+			}
 		}
-	}
-};
+	};
 
-//y data is treated as Number type.
-function type(d) {
-  d.y = +d.y;
-  return d;
-};
+	//y data is treated as Number type.
+	function type(d) {
+	  d.y = +d.y;
+	  return d;
+	};
+	
+	//Builds the season table for the particular team selected.
+	function buildTable(teamData) {
+		var abbr = "";
+			
+		if(teamData.abbr) {
+			abbr = teamData.abbr;
+		} else {
+			abbr = teamData.x;
+		}
+		
+		d3.tsv("data/" + abbr + "season.tsv", null, function(error, data) {
+
+			processResults(data, teamData);
+			
+			var columns = ["match", "date", "hteam", "result", "ateam", "loc"];
+			var table = d3.select("#seasonTable").append("table"),
+				tbody = table.append("tbody");
+				
+			var rows = tbody.selectAll("tr")
+				.data(data)
+				.enter()
+				.append("tr")
+					.attr("class", function(d) { 
+						return d.outcome; 
+					});
+				
+			var cells = rows.selectAll("td")
+				.data(function(row) {
+					return columns.map(function(column) {
+						return {column: column, value: row[column]};
+					});
+				})
+				.enter()
+				.append("td")
+					.text(function(d) { return d.value.trim(); });
+		});
+	};
+	
+	//Builds the bar chart.  Can pass in args to select they type of data and axis names for the bar chart.
+	function buildBarChart(id, dataUrl, selectedTeam, yAxisName, xAxisName) {
+		var margin = {top: 20, right: 20, bottom: 50, left: 40},
+		width = 750 - margin.left - margin.right,
+		height = 350 - margin.top - margin.bottom;
+
+		var x = d3.scale.ordinal()
+			.rangeRoundBands([0, width], .1);
+
+		var y = d3.scale.linear()
+			.range([height, 0]);
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left")
+			.ticks(10);
+
+		var svg = d3.select(id).insert("svg:svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		d3.tsv("data/" + dataUrl, type, function(error, data) {
+		  x.domain(data.map(function(d) { return d.x; }));
+		  y.domain([0, d3.max(data, function(d) { return d.y; })]);
+
+		  svg.append("g")
+			  .attr("class", "x axis")
+			  .attr("transform", "translate(0," + height + ")")
+			  .call(xAxis)
+			.append("text")
+			  .attr("x", width/2)
+			  .attr("y", 40)
+			  .style("text-anchor", "end")
+			  .text(xAxisName);
+
+		  svg.append("g")
+			  .attr("class", "y axis")
+			  .call(yAxis)
+			.append("text")
+			  .attr("y", -10)
+			  .style("text-anchor", "middle")
+			  .text(yAxisName);
+
+		  svg.selectAll(".bar")
+			  .data(data)
+			.enter().append("rect")
+			  .attr("class", function(d) {
+					if(d.x === selectedTeam) {
+						return "selectedBar";
+					}
+					
+					return "bar";
+			  })
+			  .attr("x", function(d) { return x(d.x); })
+			  .attr("width", x.rangeBand())
+			  .attr("y", function(d) { return y(d.y); })
+			  .attr("height", function(d) { return height - y(d.y); })
+			  .on("click", function(d) { barClick(d); })
+			  .on("mouseover", function(d){return showLabel(d);})
+			  .on("mouseout", function(d){return hideLabel();});
+		});
+	};
+	
+	return {
+	
+		//Wraps buildTable for other modules to use.
+		buildSeasonTable: function(teamData) {
+			buildTable(teamData);
+		},
+		
+		//Wraps buildBarChart for other modules to use.
+		buildBar: function(id, dataUrl, selectedTeam, yAxisName, xAxisName) {
+			buildBarChart(id, dataUrl, selectedTeam, yAxisName, xAxisName);
+		}
+	};
+});
